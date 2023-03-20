4c8a2688f94c32bd2bc7559d5bf7253c7ef33072
Israel Borges <israelpborges@gmail.com>
2023-03-19 23:31:20 -0300

    criando exerc. 008

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,4 +1,6 @@
-var vel = 60.5
+var vel = 78.12
 console.log('A velocidade do seu carro é ' + vel + ' Km/h')
-console.log('Você ultrapassou a velocidade permitida. Multado!')
+if (vel > 60) { // Condição simples!
+    console.log('Você ultrapassou a velocidade permitida. Multado!')
+}
 console.log('Dirija sempre usando cinto de segurança!')
